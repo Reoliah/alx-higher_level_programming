4c3325f6959310fa3f9437eb3243f825808aec9b
Isaiah <tenidaisy@gmail.com>
2022-09-26 08:39:28 -0700

    data structures

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3 
- a = 89 
- b = 10 
- c = b
- d = a
- a = c
- b = d
- print("a={:d} - b={:d}".format(a, b))
+#!/usr/bin/python3
+a = 89
+b = 10
+c, d = a, b
+a, b = d, c
+print("a= {:d} - b= {:d}".format(a,b))
