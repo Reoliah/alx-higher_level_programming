5a76d691db203c0459e3ebed23b74179506cd45a
Isaiah <tenidaisy@gmail.com>
2022-09-28 22:19:39 +0000

    Sets and Dictionaries

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for nm in my_list:
-	if j == search:
-		new_line.append(replace)
-	else:
-		new_line.append(j)
-    return new_list
+	new_list = []
+	for nm in my_list:
+		if j == search:
+			new_line.append(replace)
+		else:
+			new_line.append(j)
+    	return new_list
