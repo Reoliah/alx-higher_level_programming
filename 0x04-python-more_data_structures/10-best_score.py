c14aace473fce0161c5df189beec7026c4b2ac46
Isaiah <tenidaisy@gmail.com>
2022-09-28 18:28:18 -0700

    Sets and Dictionaries

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     val = list(a_dictionary.values())
-    key = list(a_dictionary)
+    key = list(a_dictionary.keys())
     return (key[val.index(max(val))])
