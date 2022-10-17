e3727d92bd6a7e30a220e534a39882896d7f8f1b
Isaiah <tenidaisy@gmail.com>
2022-10-17 06:00:48 -0700

    OOP in python

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,8 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 Creating an empty Rectangle class in Python
 """
 class Rectangle:
-    # naming the class
+    """ naming the class """
     pass
-# pass makes the interpreter skip the class
+"""  pass makes the interpreter skip the class """
